9fa25e774cbd9bcb974b11e5bb88b0918ac478f7
giusegit <37208205+giusegit@users.noreply.github.com>
2024-09-21 16:36:15 +0200

    Update BlueprintToCpp.Target.cs

--- Source/BlueprintToCpp.Target.cs
+++ Source/BlueprintToCpp.Target.cs
@@ -8,8 +8,8 @@ public class BlueprintToCppTarget : TargetRules
 	public BlueprintToCppTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("BlueprintToCpp");
 	}
 }
